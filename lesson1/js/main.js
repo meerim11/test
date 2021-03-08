9d2ba837538861d739039031da2f5c0ab036d69d
meerim11 <mashaivanova9898@bk.ru>
2021-03-08 13:29:24 +0600

    first commit

---
+++ lesson1/js/main.js
@@ -0,0 +1,24 @@
+let nm1 = +prompt('Введите первое число');
+let nm2 = +prompt('введите второе число');
+
+alert('${nm1 + nm2}, ${nm1 - nm2}, ${nm1 * nm2}, ${nm1 / nm2}')
+
+// let sum = nm1 + nm2;
+// let min = nm1 - nm2;
+// let multiply = nm1 * nm2;
+// let del = nm1 / nm2;
+
+
+
+
+// alert(sum)
+// alert(min)
+// alert(multiply)
+// alert(del)
+
+let a6 = 5 % 3;
+let a7 = 3 % 3;
+let a8 = 5 + '3';
+let a9 = '5' - 3;
+let a10 = 75 + 'кг';
+console.log(a6, a7, a8, a9, a10)
